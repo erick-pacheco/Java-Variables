020db3240fa14590f3042500412e1da735093fe0
Erick Pacheco <58119210+erick-pacheco@users.noreply.github.com>
2022-11-20 02:43:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+
+    /* Declaration of a variable */
+    String message;
+
+    /* Assign value to a variable */
+    message = "Want to learn more about variables?";
+
+    /* Initialize a variable */
+    String initialized = "visit https://www.w3schools.com/java/java_variables.asp";
+
+    /* Accessing a variable's value */
+    System.out.println(message);
+    System.out.println(initialized);
+  }
+}
